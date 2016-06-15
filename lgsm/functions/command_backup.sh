18f52b7f43781302c52407b6881f4c7f8dfcc718
Blacktiger <lpzockerrmc@gmail.com>
2016-06-15 14:17:32 +0200

    Update command_backup.sh

--- lgsm/functions/command_backup.sh
+++ lgsm/functions/command_backup.sh
@@ -15,28 +15,12 @@ echo ""
 fn_print_info_nl "A total of $(du -sh "${rootdir}" --exclude="${backupdir}" | awk '{print $1}') will be compressed into the following backup:"
 echo "${backupdir}/${backupname}.tar.gz"
 echo ""
-while true; do
-	read -p "Continue? [Y/N]" yn
-	case $yn in
-	[Yy]* ) break;;
-	[Nn]* ) echo Exiting; return;;
-	* ) echo "Please answer yes or no.";;
+
 esac
 done
 echo ""
 check_status.sh
-if [ "${status}" != "0" ]; then
-	echo ""
-	fn_print_warning_nl "${servicename} is currently running."
-	sleep 1
-	while true; do
-		read -p "Stop ${servicename} while running the backup? [Y/N]" yn
-		case $yn in
-		[Yy]* ) command_stop.sh; break;;
-		[Nn]* ) break;;
-		* ) echo "Please answer yes or no.";;
-	esac
-	done
+
 fi
 fn_scriptlog "Started backup"
 fn_print_dots "Backup in progress, please wait..."
